b40a858bcf6583347bbc8242788ca26a9d644cd5
sasugadayo <sasugaff@icloud.com>
2020-04-22 17:58:36 +0900

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,45 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+// ここから追加
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faUser,
+  faSignOutAlt,
+  faEllipsisV,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faUser, faSignOutAlt, faEllipsisV);
+
+import { firestorePlugin } from "vuefire";
+import firebase from "firebase";
+import "firebase/firestore";
+
+Vue.use(firestorePlugin);
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCjGdurRkqIPvLB-dXuIVQVzm6GvUCwMSg",
+  authDomain: "whisperer-4ba36.firebaseapp.com",
+  databaseURL: "https://whisperer-4ba36.firebaseio.com",
+  projectId: "whisperer-4ba36",
+  storageBucket: "whisperer-4ba36.appspot.com",
+  messagingSenderId: "123182232947",
+  appId: "1:123182232947:web:b2e0a4a2c095798ba18106",
+});
+
+export const db = firebase.firestore();
+export const auth = firebase.auth();
+
+Vue.component("fa", FontAwesomeIcon);
+// ここまで追加
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+  render: function(h) {
+    return h(App);
+  },
+}).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,89 @@
+<!-- Home.vue -->
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="list">
+      <li
+        v-for="whisper in orderBy(whispers, 'date', -1)"
+        :key="whisper.id"
+        class="item"
+      >
+        <!-- ここから削除
+        {{whisper.content}}
+        {{whisper.uid}}
+        ここまで削除  -->
+
+        <!-- ここを追加 -->
+        <div class="user-box">
+          <div
+            class="avatar"
+            :style="'background-image: url(' + url + ')'"
+          ></div>
+          <p class="user-name">{{ whisper.uid }}</p>
+        </div>
+        <div class="content" v-html="whisper.content"></div>
+        <!-- ここまで追加 -->
+      </li>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { db } from "../main";
+
+// ここを追加
+import Vue2Filters from "vue2-filters";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  data() {
+    return {
+      whispers: [],
+    };
+  },
+  firestore() {
+    return {
+      whispers: db.collection("whispers"),
+    };
+  },
+  components: {},
+
+  // ここを追加
+  mixins: [Vue2Filters.mixin],
+};
 </script>
+
+<style lang="stylus" scoped>
+
+/* ここから追加 */
+.item
+  list-style none
+  border-top 1px solid #eee
+  padding 5px 15px
+  display flex
+  flex-wrap no-wrap
+  justify-content flex-start
+  position relative
+  &:first-child
+    border none
+  &:hover
+    background rgba(0,0,0,.02)
+  .user-box
+    margin 10px 10px 10px 0
+    .avatar
+      height 50px
+      width 50px
+      border-radius 50%
+      border 1px solid #eee
+      background-size cover
+    .user-name
+      margin 5px 0 0 0
+      text-align center
+      font-size .7rem
+      line-height .7rem
+      width 50px
+  .content
+    padding 10px
+/* ここまで追加 */
+</style>
